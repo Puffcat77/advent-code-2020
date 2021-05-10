7acd92b2cb01ce5063513476a2a19328f5410edb
Акопян Александр Айрапетович <akopia-sasha@mail.ru>
2021-05-11 02:59:13 +0300

    add day 4 solution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from day_1 import task as task1
 from day_2 import task as task2
 from day_3 import task as task3
+from day_4 import task as task4
 
 
 def getData(path):
@@ -13,4 +14,5 @@ def getData(path):
 if __name__ == '__main__':
     # task1.task(getData('day_1/input.txt'))
     # task2.task(getData('day_2/input.txt'))
-    task3.task(getData('day_3/input.txt'))
+    # task3.task(getData('day_3/input.txt'))
+    task4.task('day_4/input.txt')
