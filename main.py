4906d1e594267458fb216620501c49f20f6967dc
Акопян Александр Айрапетович <akopia-sasha@mail.ru>
2021-05-11 04:19:55 +0300

    add day 5 solution

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from day_1 import task as task1
 from day_2 import task as task2
 from day_3 import task as task3
 from day_4 import task as task4
+from day_5 import task as task5
 
 
 def getData(path):
@@ -15,4 +16,5 @@ if __name__ == '__main__':
     # task1.task(getData('day_1/input.txt'))
     # task2.task(getData('day_2/input.txt'))
     # task3.task(getData('day_3/input.txt'))
-    task4.task('day_4/input.txt')
+    # task4.task('day_4/input.txt')
+    task5.task(getData('day_5/input.txt'))
